6366ff44b71de2ae75ef56a0fc601c043a05db07
Mohamed Tarek <m.tarek@llp.local>
2025-12-15 17:19:31 +0200

    feat: add WorkflowSection component to enhance app functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import FeatureSection from "./components/FeatureSection";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import WorkflowSection from "./components/WorkflowSection";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="max-w-5xl mx-auto pt-20 px-6">
         <HeroSection />
         <FeatureSection />
+        <WorkflowSection />
       </div>
     </>
   );
